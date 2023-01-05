388a1496a7ea3dd53121ba8d7bd1eaee76207301
fatmadurak <fatmabadillidurak@gmail.com>
2023-01-05 13:57:14 +0300

    routing commited

---
+++ src/Pages/Users.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+
+function Users() {
+
+    const [users,setUsers]=useState([]);
+
+   useEffect(()=>{
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res)=>res.json())
+    .then((data)=>setUsers(data))
+    
+
+
+   },[])
+
+  return (
+    <div>
+        <h1>Kullanıcılar</h1>
+    
+
+        <ul>
+          {users.map(user => (
+            <li key={user.id}>
+           <Link to={`/users/${user.id}`}> {user.name} </Link>
+            </li>
+          ))}
+     
+     </ul>
+    </div>
+  )
+}
+
+export default Users
