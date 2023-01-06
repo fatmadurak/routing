cf02016f169aa183c2964cb21735203f70e09275
fatmadurak <fatmabadillidurak@gmail.com>
2023-01-06 11:46:14 +0300

    commited

--- src/Pages/Dashboard/Contact.js
+++ src/Pages/Dashboard/Contact.js
@@ -1,10 +1,69 @@
 import React from 'react'
+import { Formik } from 'formik';
 
 function Contact() {
   return (
     <div>
 
      <h1>İletişim</h1>
+     <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        email: '',
+        message:"",
+      }}
+      onSubmit={async (values) => {
+ 
+        console.log(values)
+      }}
+    >
+    {
+    
+    (({handleSubmit,handleChange})=>(
+
+      <form onSubmit={handleSubmit} className="form">
+        <div>
+        <label htmlFor="firstName">First Name</label>
+        <input id="firstName" name="firstName" placeholder="Jane"  onChange={handleChange("firstName")}/>
+        </div>
+
+     <div>
+     <label htmlFor="lastName">Last Name</label>
+        <input id="lastName" name="lastName" placeholder="Doe"  onChange={handleChange("lastName")}/>
+     </div>
+      <div>
+  
+       <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          name="email"
+          placeholder="jane@acme.com"
+          type="email"
+          onChange={handleChange("email")}
+        />
+       </div>
+         <div>
+          <label htmlFor="message">Message</label>
+          <textarea
+          id="message"
+          name="message"
+          placeholder="Your message..."
+          onChange={handleChange("message")}
+           />
+          </div>
+        <button type="submit">Submit</button>
+      </form>
+
+     ))
+
+
+
+
+
+
+    }
+    </Formik>
 
     </div>
   )
